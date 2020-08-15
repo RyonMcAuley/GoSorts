ad4ec209b4bb127a46120ee2b7c8f932d277b4ba
Ryon McAuley <ryonmcauley@Ryons-MacBook-Pro.local>
2020-08-15 02:14:25 -0400

    basic sorts package

---
+++ sorts.go
@@ -0,0 +1,53 @@
+package mesorts
+
+type Sorting interface {
+	//Perform a Selection Sort
+	SelectionSort(arr []int)
+	QuickSort(arr []int, low int, high int)
+}
+
+type Sorter struct {
+	Arr []int
+	Len int
+}
+
+func (s *Sorter) SelectionSort(arr []int) {
+	for i := 0; i < s.Len-1; i++ {
+		mindex := i
+		for j := i + 1; j < s.Len; j++ {
+			if arr[j] < arr[mindex] {
+				mindex = j
+			}
+		}
+		swap(&arr[mindex], &arr[i])
+	}
+}
+
+//Helper function for various sorts
+func swap(xp *int, yp *int) {
+	tmp := *xp
+	*xp = *yp
+	*yp = tmp
+}
+
+func (s *Sorter) QuickSort(arr []int, low int, high int)  {
+    if low < high {
+        pi := partition(arr, low, high)
+        s.QuickSort(arr, low, pi-1)
+        s.QuickSort(arr, pi+1, high)
+    }
+}
+
+func partition(arr []int, low int, high int) int {
+    pivot := arr[high]
+    i := low-1
+
+    for j:=low; j < high; j++ {
+        if arr[j] < pivot {
+            i++
+            swap(&arr[i], &arr[j])
+        }
+    }
+    swap(&arr[i+1], &arr[high])
+    return i+1
+}
